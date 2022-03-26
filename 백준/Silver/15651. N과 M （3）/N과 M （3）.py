b092e16ce594840e826e19ab0cd785192973989e
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-03-26 16:41:07 +0900

    [Silver III] Title: N과 M (3), Time: 1508 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+def dfs(i, N, M):
+    if i == M:
+        print(' '.join(map(str, stack)))
+    else:
+        for j in range(1, N+1):
+            stack.append(j)
+            dfs(i+1, N, M)
+            stack.pop()
+
+
+N, M = map(int, input().split())
+stack = []
+dfs(0, N, M)
