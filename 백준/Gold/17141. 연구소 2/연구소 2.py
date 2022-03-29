6ba13400fa0333e74f76ba3e2e127c94f3b5be71
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-03-29 23:02:05 +0900

    [Gold IV] Title: 연구소 2, Time: 296 ms, Memory: 129524 KB -BaekjoonHub

--- 백준/Gold/17141. 연구소 2/연구소 2.py
+++ 백준/Gold/17141. 연구소 2/연구소 2.py
@@ -1,67 +1,69 @@
+import sys
 from collections import deque
-from itertools import combinations
 
-def bfs(elem):
-    global aisle
+def bfs(lst_v): #바이러스1,2,3 위치
+    global time
+    q = deque()
+    visited = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if matrix[i][j] == 1:
+                visited[i][j] = '-'
 
-    q = deque(elem)
-    for k in q:
-        visited[k[0]][k[1]] = 1
+    for i in lst_v:
+        q.append(vi_lst[i])
+        r,c = vi_lst[i]
+        visited[r][c] = 1
 
     while q:
-        r, c = q.popleft()
+        r,c = q.popleft()
+
         for i in range(4):
-            rr = r + dr[i]
-            cc = c + dc[i]
-            if 0 <= rr < N and 0 <= cc < N and matrix[rr][cc] != '-' and not visited[rr][cc]:
-                visited[rr][cc] = visited[r][c] + 1
-                aisle -= 1
-                q.append([rr, cc])
+            rr, cc = r+dr[i], c+dc[i]
+            if 0 <= rr < N and 0 <= cc < N and not visited[rr][cc]:
+                visited[rr][cc]= visited[r][c]+1
+                q.append((rr,cc))
 
-    return visited[r][c] - 1
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == 0:
+                break
+            else:
+                time = min(time, visited[r][c] - 1)
+        if visited[i][j] == 0:
+            time = -1
+            break
 
+    return
 
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-min_time = 987654321
-# 0 : 바이러스가 퍼질 수 있는 곳
-# 1 : 벽, 바이러스가 퍼질 수 없음
-# 2 : 바이러스의 초기 위치가 될 수 있음
+def dfs(m,n,lst):
+    global time
+    if len(lst) == m:
+        time =99999
+        bfs(lst)
+        time_lst.append(time)
+        return
+    for k in range(n):
+        dfs(m, k, lst+[k])
 
-# delta 상 하 좌 우
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
+N, M = map(int,input().split())
+#N크기, M은 바이러스의 수
 
-# 바이러스가 출발할 수 있는 위치들 넣어줄 list
-virus = []
-aisle = N*N - M # 바이러스가 지나다닐 수 있는 통로 수를 세어주자
-# 벽인 부분은 헷갈리지 않도록 문자열로 바꿔주자
+matrix = [list(map(int,input().split())) for _ in range(N)]
+vi_lst = []
+dr,dc = [-1,1,0,0],[0,0,-1,1]
 for i in range(N):
     for j in range(N):
-        if matrix[i][j] == 1:
-            matrix[i][j] = '-'
-            aisle -= 1  # 벽은 통로에서 제외해줘
         if matrix[i][j] == 2:
-            virus.append([i, j])
+            vi_lst.append((i, j))
+time_lst = []
+dfs(M,len(vi_lst), [])
 
-# pprint(matrix)
-# print(aisle)
-tmp = aisle
-# virus 출발 가능한 위치에서 M개 뽑을 수 있는 경우의 수에서
-for start in combinations(virus, M):
-    aisle = tmp
-    visited = [[0] * N for _ in range(N)]
-
-    # q = deque(start)
-    # for k in q:
-    #     visited[k[0]][k[1]] = 1
-    time = bfs(start)  # bfs 돌려서 최종 소요 시간 구해주자
-
-    if time < min_time and aisle == 0:
-        min_time = time
-
-if min_time == 987654321:
+if sum(time_lst) == -len(time_lst):
     print(-1)
 else:
-    print(min_time)
+    ans = N * N
+    for item in time_lst:
+        if item > -1 and ans > item:
+            ans = item
+    print(ans)
