8959a31c44663fdf964fecc99582975de8dd3c73
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-04-13 13:06:55 +0900

    [Gold IV] Title: 집합의 표현, Time: 4516 ms, Memory: 141208 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(100000)
+
+def union(x, y):
+    i = find(x)
+    j = find(y)
+
+    parent[max(i, j)] = min(i, j)
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+
+    return parent[x]
+
+
+n, m = map(int, input().split())
+info = [list(map(int, input().split())) for _ in range(m)]
+parent = [x for x in range(n+1)]  # 0 부터 n까지 !
+q = deque(info)
+
+while q:
+    check, a, b = q.popleft()
+    if check == 0:
+        union(a, b)
+    if check == 1:
+        if find(a) != find(b):
+            print('NO')
+        else:
+            print('YES')
