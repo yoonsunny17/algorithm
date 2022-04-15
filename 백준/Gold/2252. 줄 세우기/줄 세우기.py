84b81416ce95dfa6598e247e18c31ed2b6424d33
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-04-16 00:26:19 +0900

    [Gold III] Title: 줄 세우기, Time: 4096 ms, Memory: 38132 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+N, M = map(int, input().split())
+
+indegree = [0]*(N+1)
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+    indegree[b] += 1
+
+result = []
+q = deque()
+
+for i in range(1, N+1):
+    if indegree[i] == 0:
+        q.append(i)
+
+while q:
+    now = q.popleft()
+    result.append(now)
+
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            q.append(i)
+
+print(*result)
