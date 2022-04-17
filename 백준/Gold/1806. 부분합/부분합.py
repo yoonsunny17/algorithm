eb3a8b6c27d297f389f6726f99c12fd9e9f7a58c
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-04-17 09:49:08 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 128152 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,28 @@
+import sys
+
+N, S = map(int, sys.stdin.readline().split())
+numbs = list(map(int, sys.stdin.readline().split()))
+
+start = 0
+end = 1
+interval_sum = numbs[0]
+min_rlt = float('inf')
+
+while True:
+    if interval_sum >= S:
+        if end - start < min_rlt:
+            min_rlt = end - start
+        interval_sum -= numbs[start]
+        start += 1
+
+    elif end == N:
+        break
+
+    else:
+        interval_sum += numbs[end]
+        end += 1
+
+if min_rlt == float('inf'):
+    print(0)
+else:
+    print(min_rlt)
