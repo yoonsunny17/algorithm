b2994513e6185a442c2185bb694783e196a41be6
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-12-12 23:18:33 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,9 @@
+function solution(chicken) {
+    let coupon = chicken
+    let service = 0
+    while (coupon >= 10) {
+        service += Math.floor(coupon / 10)
+        coupon = coupon%10 + Math.floor(coupon/10)
+    }
+    return service
+}
