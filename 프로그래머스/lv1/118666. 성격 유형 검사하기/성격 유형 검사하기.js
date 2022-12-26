75a3f9d08b7237395259aa26f6a2ab452026146d
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2022-12-26 15:24:27 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.98 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,27 @@
+function solution(survey, choices) {
+    const types = {R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N:0}
+    let answer = ''
+    for (let i=0; i<choices.length; i++){
+        const [leftType, rightType] = survey[i].split('')
+        const result = Math.abs(choices[i] - 4)
+        if (choices[i] < 4){
+            types[leftType] += result
+        } else if (choices[i] > 4){
+            types[rightType] += result
+        }
+    }
+    const type = Object.keys(types)
+    
+    for (let i=0; i<type.length; i+=2){
+        const leftT = types[type[i]]
+        const rightT = types[type[i+1]]
+        
+        if (leftT >= rightT){
+            answer += type[i]
+        } else {
+            answer += type[i+1]
+        }
+    }
+    
+    return answer
+}
