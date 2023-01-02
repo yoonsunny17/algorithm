5e06e861fa281311d2e9a339d6b7f133d062e4f7
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-02 17:46:11 +0900

    [Gold V] Title: 토마토, Time: 2520 ms, Memory: 60612 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,18 +1,22 @@
 from collections import deque
 
 M, N, H = map(int, input().split())
+matrix = [list(list(map(int, input().split())) for _ in range(N)) for _ in range(H)]
+visited = [list([0] * M for _ in range(N)) for _ in range(H)]
 
-box = [list(list(map(int, input().split())) for _ in range(N)) for _ in range(H)]
+day = 0
 
+# 상 하 좌 우 앞 뒤
 dx = [0, 0, -1, 1, 0, 0]
 dy = [0, 0, 0, 0, 1, -1]
 dz = [1, -1, 0, 0, 0, 0]
 
 q = deque()
+
 for k in range(H):
     for j in range(N):
         for i in range(M):
-            if box[k][j][i] == 1:
+            if matrix[k][j][i] == 1:
                 q.append([k, j, i])
 
 def bfs():
@@ -22,23 +26,22 @@ def bfs():
             zz = z + dz[i]
             yy = y + dy[i]
             xx = x + dx[i]
-            if 0 <= xx < M and 0 <= yy < N and 0 <= zz < H and not box[zz][yy][xx]:
-                box[zz][yy][xx] = box[z][y][x] + 1
+            # 범위 내애 존재하고, 아직 익지 않은 토마토인 경우(= 0)
+            if 0 <= xx < M and 0 <= yy < N and 0 <= zz < H and matrix[zz][yy][xx] == 0:
+                matrix[zz][yy][xx] = matrix[z][y][x] + 1
                 q.append([zz, yy, xx])
 
-    return box
+    return matrix
 
 bfs()
-rlt = 0
 
-for i in box:
-    for j in i:
-        for k in j:
-            if k == 0:
+for a in matrix:
+    for b in a:
+        for c in b:
+            if c == 0:
                 print(-1)
                 exit(0)
             else:
-                if k > rlt:
-                    rlt = k
+                day = max(day, c)
 
-print(rlt - 1)
+print(day-1)
