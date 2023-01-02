45431371ee3e92c766f4ab7bc8c77cd079d8a091
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-02 17:10:23 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,13 +1,13 @@
 from collections import deque
 
-N, M = map(int, input().split())
-matrix = [list(map(int, input())) for _ in range(N)]
-visited = [[0]*M for _ in range(N)]
+n, m = map(int, input().split())
+matrix = [list(map(int, input())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
 
+# 상 하 좌 우 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-
-
+ 
 def bfs():
     q = deque()
     q.append([0, 0])
@@ -18,11 +18,16 @@ def bfs():
         for i in range(4):
             rr = r + dr[i]
             cc = c + dc[i]
-            if 0 <= rr < N and 0 <= cc < M and matrix[rr][cc] == 1 and not visited[rr][cc]:
+            # rr, cc 가 범위 내에 존재하고, matrix에서 1이고 아직 방문 안했으면
+            if 0 <= rr < n and 0 <= cc < m and matrix[rr][cc] == 1 and not visited[rr][cc]:
+                # 방문 처리 해줘 근데 누적으로
                 visited[rr][cc] = visited[r][c] + 1
-                if rr == N-1 and cc == M-1:
+                # 만약에 도착지점에 도달했으면 리턴해줘
+                if rr == n-1 and cc == m-1:
                     return visited[rr][cc]
+                # 다음 탐색 지점 넣어줘
                 q.append([rr, cc])
 
 rlt = bfs()
+
 print(rlt)
