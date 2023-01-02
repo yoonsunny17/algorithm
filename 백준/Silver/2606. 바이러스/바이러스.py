371b1056d2a91adab6d5682b5f24b0b4f59790a2
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-02 16:28:10 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,6 +1,7 @@
 def dfs(start):
     visited[start] = 1
     global cnt
+
     for i in graph[start]:
         if not visited[i]:
             dfs(i)
@@ -10,11 +11,13 @@ N = int(input())
 M = int(input())
 graph = [[] for _ in range(N+1)]
 visited = [0]*(N+1)
+
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 cnt = 0
+
 dfs(start=1)
 print(cnt)
