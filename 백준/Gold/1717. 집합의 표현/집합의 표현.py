13aa1abc7e5663eaa7789dc3bb267a86aa962236
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-03 13:58:57 +0900

    [Gold IV] Title: 집합의 표현, Time: 308 ms, Memory: 75960 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,29 +1,30 @@
 import sys
-from collections import deque
 sys.setrecursionlimit(100000)
 
-def union(x, y):
-    i = find(x)
-    j = find(y)
 
-    parent[max(i, j)] = min(i, j)
+def union(x, y):
+    a = find(x)
+    b = find(y)
+    
+    parent[max(a, b)] = min(a, b)
 
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
-
+    
     return parent[x]
 
-
 n, m = map(int, sys.stdin.readline().split())
-info = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
-parent = [x for x in range(n+1)]  # 0 부터 n까지 !
-q = deque(info)
+parent = [x for x in range(n+1)]
 
-while q:
-    check, a, b = q.popleft()
+for _ in range(m):
+    check, a, b = map(int, sys.stdin.readline().split())
+
+    # check 가 0이면 합집합 연산 고고 (union)
     if check == 0:
         union(a, b)
+    
+    # check 가 1이면 a, b가 같은 집합에 있는지 확인해주자 (find)
     if check == 1:
         if find(a) != find(b):
             print('NO')
