a686a66b1a755472dd0c5fbbd26e76c9c2522fd1
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-16 22:21:01 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,8 +1,15 @@
 from collections import deque
 
-def bfs(x, y, graph):
+N = int(input())
+matrix = [list(map(int, input())) for _ in range(N)]
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+def bfs(matrix, r, c):
     q = deque()
-    q.append([x, y])
+    q.append([r, c])
+    matrix[r][c] = 0
     cnt = 1
 
     while q:
@@ -11,33 +18,14 @@ def bfs(x, y, graph):
             rr = r + dr[i]
             cc = c + dc[i]
             if 0 <= rr < N and 0 <= cc < N and matrix[rr][cc] == 1:
-                matrix[rr][cc] = 0
-                q.append([rr, cc])
                 cnt += 1
-
+                q.append([rr, cc])
+                matrix[rr][cc] = 0
+            
     return cnt
 
-N = int(input())
-matrix = [list(map(int, input())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
-rlt = []
-
-max_numb = 0
-for i in range(N):
-    for j in range(N):
-        if matrix[i][j] == 1:
-            numb = bfs(i, j, matrix)
-            if numb == 1:
-                rlt.append(1)
-            else:
-                rlt.append(numb-1)
-
-rlt.sort()
-
-print(len(rlt))
-for i in rlt:
-    print(i)
+arr = [bfs(matrix, r, c) for r in range(N) for c in range(N) if matrix[r][c] == 1]
+arr.sort()
+print(len(arr))
+for a in arr:
+    print(a)
