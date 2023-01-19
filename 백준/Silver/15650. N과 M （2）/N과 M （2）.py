61f79971b47e636c9ce81da37df5697c1f7fe52c
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-01-19 23:37:11 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,10 +1,14 @@
-from itertools import combinations
+def dfs(start):
+    if len(stack) == M:
+        print(' '.join(map(str, stack)))
+        return
+    else:
+        for i in range(start, N+1):
+            if i not in stack:
+                stack.append(i)
+                dfs(i+1)
+                stack.pop()
 
 N, M = map(int, input().split())
-numbs = [x for x in range(1, N+1)]
-
-lst = list(combinations(numbs, M))
-
-for i in lst:
-    rlt = ' '.join(map(str, i))
-    print(rlt)
+stack = []
+dfs(1)
