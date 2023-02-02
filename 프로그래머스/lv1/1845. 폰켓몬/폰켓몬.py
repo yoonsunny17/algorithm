a0147532caeea20c002483c9e9c34dcb6f837d31
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-02-02 20:48:31 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,6 @@
+def solution(nums):
+    want = len(nums) // 2  # 내가 뽑고 싶은 포켓몬 수
+    kinds = len(set(nums))  # 포켓몬 종류 개수
+    
+    # 종류가 다양하면 내가 뽑고 싶은 포켓몬 수만큼 종류 다양하게 뽑을 수 있음
+    return want if kinds >= want else kinds
