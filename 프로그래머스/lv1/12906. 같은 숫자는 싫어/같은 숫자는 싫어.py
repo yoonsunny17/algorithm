4480251d865b7a8aeaf725a87ee21927ac9eade6
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-02-02 22:23:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 105.19 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,18 @@
+# def solution(arr):
+#     answer = []
+#     answer.append(arr[0])
+#     for i in range(1, len(arr)):
+#         if arr[i] != arr[i-1]:
+#             answer.append(arr[i])
+
+#     return answer
+
 def solution(arr):
     answer = []
-    answer.append(arr[0])
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
-            answer.append(arr[i])
-
+    
+    for i in arr:
+        # stack이 비어있거나 or stack 마지막에 들어간 숫자랑 다른 숫자면 넣어줘
+        if (len(answer) == 0) or (answer[-1] != i):
+            answer.append(i)
+            
     return answer
