2157ecc338b896e124c66cc37e584b74b2e6eec9
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-02-03 11:34:05 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,50 +1,52 @@
 from collections import deque
 
-# delta 상 하 좌 우
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-
-def bfs(matrix, x, y, visited):
-    q.append([x, y])
-    visited[x][y] = 1
+def bfs(r, c, visited):
+    q = deque()
+    q.append([r, c])
+    visited[r][c] = 1
 
     while q:
         r, c = q.popleft()
         for i in range(4):
             rr = r + dr[i]
             cc = c + dc[i]
-            # 탐색 지점이 범위 내에 존재하고, 이전 영역과 색이 동일하며 아직 방문한 적이 없다면,
-            if 0 <= rr < N and 0 <= cc < N and matrix[rr][cc] == matrix[r][c] and not visited[rr][cc]:
-                visited[rr][cc] = 1  # 방문 기록을 남겨주고
-                q.append([rr, cc])  # q에 경로를 추가해줘
+            # 탐색할 지점이 범위 내에 존재하고, 아직 방문한 적 없으면서, 지금 지점이랑 같은 색이라면
+            if 0 <= rr < n and 0 <= cc < n and not visited[rr][cc] and matrix[r][c] == matrix[rr][cc]:
+                # 방문 처리 해주고 다음 탐색지점으로 넣어주자
+                visited[rr][cc] = 1
+                q.append([rr, cc])
 
 
-N = int(input())
-matrix = [list(input()) for _ in range(N)]
-q = deque()
+n = int(input())
+matrix = [list(input()) for _ in range(n)]
 
-# case 1. 색약이 아닌 경우
-cnt1 = 0
-visited1 = [[0]*N for _ in range(N)]
-for i in range(N):
-    for j in range(N):
+# delta
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+# case1. 적록색약 아닌 경우
+cnt1 = 0  # 구역 몇개인지 세어줄 변수
+visited1 = [[0]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
         if not visited1[i][j]:
-            bfs(matrix, i, j, visited1)
+            bfs(i, j, visited1)
+            # bfs 빠져나왔다 = 인접 구역에 같은색 없다
             cnt1 += 1
 
-# case 2. 색약인 경우; G를 R로 인식한다고 가정
-# matrix 먼저 바꿔주자
-for i in range(N):
-    for j in range(N):
+# case2. 적록색약인 경우
+# G -> R로 바꿔주고 bfs 돌려주자
+cnt2 = 0  # 구역 몇개인지 세어줄 변수
+visited2 = [[0]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
         if matrix[i][j] == 'G':
             matrix[i][j] = 'R'
 
-cnt2 = 0
-visited2 = [[0]*N for _ in range(N)]
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(n):
         if not visited2[i][j]:
-            bfs(matrix, i, j, visited2)
+            bfs(i, j, visited2)
             cnt2 += 1
 
-print(cnt1, cnt2)
+print(f'{cnt1} {cnt2}')
