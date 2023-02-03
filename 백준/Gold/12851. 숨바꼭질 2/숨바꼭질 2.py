8cd192cc391354b3cc990db820dc223ea67df7db
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-02-03 14:15:26 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 696 ms, Memory: 37552 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+N, K = map(int, input().split())
+MAX_SIZE = 100001
+
+q = deque()
+q.append(N)
+
+cnt=0
+check=[-1]* MAX_SIZE
+check[N]=0
+while q:
+    x = q.popleft()
+    if x==K:
+        cnt+=1
+    for y in [x * 2, x + 1, x - 1]:
+        if 0 <= y < MAX_SIZE:
+            if check[y]==-1 or check[y]==check[x]+1: #시간: 방문한 적 없거나 현재시간 +1인경우
+                check[y]=check[x]+1
+                q.append(y)
+
+print(check[K])
+print(cnt)
