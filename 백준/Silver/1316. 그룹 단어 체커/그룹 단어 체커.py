6d948839548152e3df6de0d1c8ea6e6ba89d71d8
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-02-23 23:06:27 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,13 @@
+n = int(input())
+rlt = n
+
+for _ in range(n):
+    word = input()
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            pass
+        elif word[i] in word[i+1:]:
+            rlt -= 1
+            break
+
+print(rlt)
