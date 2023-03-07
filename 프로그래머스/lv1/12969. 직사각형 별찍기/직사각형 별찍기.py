fd7afc60624efab720c3ea658410a993ae777a33
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-03-07 23:48:12 +0900

    [level 1] Title: 직사각형 별찍기, Time: 32.93 ms, Memory: 7.65 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -0,0 +1,8 @@
+# a, b = map(int, input().strip().split(' '))
+# print(a + b)
+
+a, b = map(int, input().strip().split(' '))
+for _ in range(b):
+    for _ in range(a):
+        print('*', end='')
+    print()
