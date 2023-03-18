a4f5f93c663a1b50803ebaab1064c0638f6ac946
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-03-18 21:16:17 +0900

    [level 2] Title: 올바른 괄호, Time: 14.96 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,17 +1,7 @@
 def solution(s):
-    stack = []
-    for i in s:
-        # 스택이 비어있는데 닫힌괄호 들어오는 경우 False return
-        if len(stack) == 0 and i == ')':
-            return False
-        
-        # 스택 맨 위에 열린괄호 있는데 닫힌괄호 들어오는 경우: pop
-        if i == ')' and stack[-1] == '(':
-            stack.pop()
-        
-        # 열린 괄호 들어오는 경우 stack에 append
-        if i == '(':
-            stack.append('(')
+    answer = True
     
-    # 다 끝났는데 남아있으면 False return
-    return False if len(stack) != 0 else True
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return True
