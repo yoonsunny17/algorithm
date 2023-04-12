b5ee551b9a51f3771d571ee34d0ea39d552f96cf
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-04-12 23:46:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.08 ms, Memory: 26 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,9 @@
 def solution(participant, completion):
     participant.sort()
     completion.sort()
-        
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
     
-    # null인 경우 배열의 맨 마지막 값 출력
-    return participant[len(participant)-1]
+    for i in range(len(completion)):
+        if completion[i] != participant[i]:
+            return participant[i]
+        
+    return participant[-1]
