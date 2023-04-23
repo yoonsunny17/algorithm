4cba1515ab80e8c213781b0ebff9c1a2870387e3
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-04-24 00:36:52 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/README.md
+++ 프로그래머스/lv2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.10 ms
+메모리: 10.4 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,27 +1,15 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    arr = [0] * len(priorities)
-    arr[location] = 1
-    q = deque(priorities)
+    queue = deque([i, v] for i, v in enumerate(priorities))
+    ans = 0
     
     while True:
-        # arr가 비어있거나, 최댓값이 0인 경우 = 원하는 값 출력된 경우
-        if not arr or max(arr) == 0:
-            break
-        # 큐값 안의 최댓값이 첫번째 값일 때
-        # 그 값 빼주고, arr값도 빼줘
-        # 그리고 프로세스 한번 수행한 것 추가 +1
-        if max(q) == q[0]:
-            q.popleft()
-            del arr[0]
-            answer += 1
-        # 원하는 목표값이 아닌 경우라면
+        temp = queue.popleft()
+        # 큐 안에 우선순위가 더 높은게 존재하는 경우가 하나라도 있으면 true가 됨
+        if any(temp[1] < q[1] for q in queue):
+            queue.append(temp)
         else:
-            q.append(q[0])
-            arr.append(arr[0])
-            q.popleft()
-            del arr[0]
-    
-    return answer
+            ans += 1
+            if temp[0] == location:
+                return ans
