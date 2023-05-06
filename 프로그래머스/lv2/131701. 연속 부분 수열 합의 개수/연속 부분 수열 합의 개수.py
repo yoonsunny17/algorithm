ed1e322e53a70b6fffa36b740ae88ed39533b4b9
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-05-06 23:41:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4205.65 ms, Memory: 43.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,10 @@
+def solution(elements):
+    answer = set()
+    
+    n = len(elements)
+    elements = elements * 2
+    
+    for i in range(n):
+        for j in range(n):
+            answer.add(sum(elements[j:j+i+1]))
+    return len(answer)
