09b85c74dac615b45f297c4d3abca5b0abae650f
yoonsunny17 <97651006+yoonsunny17@users.noreply.github.com>
2023-05-11 10:06:09 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,14 +1,27 @@
+from collections import Counter
+
 def solution(want, number, discount):
     ans = 0
-    
-    for i in range(len(discount)):
-        ten = discount[i:i+10]
-        if len(ten) == 10:
-            cnt = 0
-            for j in range(len(want)):
-                if ten.count(want[j]) != number[j]:
-                    break
-                cnt += 1
-            if cnt == len(want):
-                ans += 1
+    dic = {}
+    for i in range(len(want)):
+        dic[want[i]] = number[i]
+    for i in range(len(discount)-9):
+        if dic == Counter(discount[i:i+10]):
+            ans += 1
     return ans
+
+
+# def solution(want, number, discount):
+#     ans = 0
+    
+#     for i in range(len(discount)):
+#         ten = discount[i:i+10]
+#         if len(ten) == 10:
+#             cnt = 0
+#             for j in range(len(want)):
+#                 if ten.count(want[j]) != number[j]:
+#                     break
+#                 cnt += 1
+#             if cnt == len(want):
+#                 ans += 1
+#     return ans
