fc42174e71117560a423b30250341b14ddad9a51
YoonSun <97651006+yoonsunny17@users.noreply.github.com>
2024-02-29 14:08:43 +0900

    [level 0] Title: 수 조작하기 2, Time: 22.31 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,13 @@
+def solution(numLog):
+    answer = ''
+    
+    for i in range(len(numLog)-1):
+        if numLog[i+1] - numLog[i] == 1:
+            answer += 'w'
+        elif numLog[i+1] - numLog[i] == -1:
+            answer += 's'
+        elif numLog[i+1] - numLog[i] == 10:
+            answer += 'd'
+        else:
+            answer += 'a'
+    return answer
