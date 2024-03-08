f1a537381982d0ccca84ef45f40a9164ceec21fb
YoonSun <97651006+yoonsunny17@users.noreply.github.com>
2024-03-08 10:26:52 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,22 @@
+total = 0
+base = 0
+score = {
+    'A+' : 4.5,
+    'A0' : 4.0,
+    'B+' : 3.5,
+    'B0' : 3.0,
+    'C+' : 2.5,
+    'C0' : 2.0,
+    'D+' : 1.5,
+    'D0' : 1.0,
+    'F' : 0.0
+}
+
+for _ in range(20):
+    subject = input().split()
+    if subject[2] == 'P':
+        continue
+    total += float(subject[1]) * score[subject[2]]
+    base += float(subject[1])
+
+print(total / base)
