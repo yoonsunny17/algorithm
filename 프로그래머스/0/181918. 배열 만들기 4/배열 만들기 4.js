879dbf7192fa3bd4c547d6a46ef0721348a5bac4
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-09-06 23:42:35 +0900

    [level 0] Title: 배열 만들기 4, Time: 26.57 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+    var stk = [];
+    let idx = 0;
+    
+    while (idx < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[idx])
+            idx++
+        } else {
+            if (stk[stk.length-1] < arr[idx]) {
+                stk.push(arr[idx])
+                idx++
+            } else {
+                stk.pop()
+            }
+        }
+    }
+    return stk;
+}
