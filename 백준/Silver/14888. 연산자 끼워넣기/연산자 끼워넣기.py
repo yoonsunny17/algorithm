0859f4cf3a5807966bd8434848744edc05ee2566
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-10-07 20:53:09 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,26 +1,27 @@
-def dfs(i, rlt, add, sub, mul, div):
-    global max_rlt, min_rlt
-
+def back(i, calc, add, sub, mlt, div):
+    global max_v, min_v
     if i == N:
-        max_rlt = max(max_rlt, rlt)
-        min_rlt = min(min_rlt, rlt)
+        max_v = max(max_v, calc)
+        min_v = min(min_v, calc)
         return
 
+    # 아직 연산자가 남아있는 경우 계산해주기
     if add > 0:
-        dfs(i+1, rlt+numbs[i], add-1, sub, mul, div)
+        back(i+1, calc+numbs[i], add-1, sub, mlt, div)
     if sub > 0:
-        dfs(i+1, rlt-numbs[i], add, sub-1, mul, div)
-    if mul > 0:
-        dfs(i+1, rlt*numbs[i], add, sub, mul-1, div)
+        back(i+1, calc-numbs[i], add, sub-1, mlt, div)
+    if mlt > 0:
+        back(i+1, calc*numbs[i], add, sub, mlt-1, div)
     if div > 0:
-        dfs(i+1, -((-rlt)//numbs[i]) if rlt < 0 else (rlt//numbs[i]), add, sub, mul, div-1)
+        back(i+1, -((-calc)//numbs[i]) if calc < 0 else calc//numbs[i], add, sub, mlt, div-1)
 
 N = int(input())
 numbs = list(map(int, input().split()))
 op = list(map(int, input().split()))
 
-max_rlt = -10**9
-min_rlt = 10**9
+max_v, min_v = -10**9, 10**9 # 최댓값, 최솟값 limit 설정
 
-dfs(1, numbs[0], op[0], op[1], op[2], op[3])
-print(f'{max_rlt}\n{min_rlt}')
+# 처음에 이미 첫번째 숫자 넣어서 계산 시작하니까 i = 0 부터 시작
+back(1, numbs[0], op[0], op[1], op[2], op[3])
+
+print(f'{max_v}\n{min_v}')
