b51f0b8e0b1e064dfbba6846fe6ef1e13e4f06c0
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-10-07 16:04:52 +0900

    [Bronze II] Title: 시험 감독, Time: 568 ms, Memory: 146464 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,15 +1,13 @@
+import math
+
 N = int(input())
-A_lst = list(map(int, input().split()))
+students = list(map(int, input().split()))
 B, C = map(int, input().split())
 
-rlt = 0
-for A in A_lst:
-    A -= B
-    rlt += 1
-    if A > 0:
-        rlt += A // C
+cnt = N
 
-        if A % C > 0:
-            rlt += 1
+for i in range(N):
+    if students[i] - B > 0:
+        cnt += math.ceil((students[i] - B) / C)
 
-print(rlt)
+print(cnt)
