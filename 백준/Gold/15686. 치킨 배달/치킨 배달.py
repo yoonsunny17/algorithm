0abb6830f864bbc563cf227a3745da8a5e892ac8
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-10-08 10:56:43 +0900

    [Gold V] Title: 치킨 배달, Time: 568 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,27 +1,31 @@
+import sys
 from itertools import combinations
+    
+N, M = map(int, sys.stdin.readline().split())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+rlt = float('inf')
 
-N, M = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-stores = []
-house = []
+# 존재하는 치킨집, 집 담아주기
+chickens, houses = [], []
 for i in range(N):
     for j in range(N):
         if matrix[i][j] == 2:
-            stores.append([i, j])
-        if matrix[i][j] == 1:
-            house.append([i, j])
+            chickens.append([i, j])
+        elif matrix[i][j] == 1:
+            houses.append([i, j])
 
-min_rlt = 987654321
-distance = 0
-for store in combinations(stores, M):
-    chicken_street = []
-    for home in house:
-        min_d = []
-        for kfc in store:
-            distance = abs(home[0]-kfc[0]) + abs(home[1]-kfc[1])
-            min_d.append(distance)
+# 선택받은 치킨집들
+for selected in combinations(chickens, M):
+    chicken_street = [] # 각 집들의 치킨거리를 담아 줄 리스트
 
-        chicken_street.append(min(min_d))
-    min_rlt = min(min_rlt, sum(chicken_street))
+    # 각 집에 대해서, 선택된 치킨가게들 중 어디가 제일 가까운지 확인해보자
+    for house in houses:
+        min_d = float('inf') # 집 하나에 대해서 치킨거리 찾아줄 변수
+        for kkk in selected:
+            dis = abs(house[0]-kkk[0]) + abs(house[1]-kkk[1])
+            min_d = min(dis, min_d)
 
-print(min_rlt)
+        chicken_street.append(min_d)
+    rlt = min(rlt, sum(chicken_street))
+
+print(rlt)
