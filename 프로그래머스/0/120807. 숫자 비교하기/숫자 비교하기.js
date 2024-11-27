db1bf38f5d94595ae6f1867ccbded0e521b61421
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-11-27 16:29:45 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,15 @@
+function solution(num1, num2) {
+    if (num1 === num2) {
+        return 1
+    } else return -1
+}
+
+// function solution(num1, num2) {
+//     var answer = 0;
+//     if (num1 === num2) {
+//         answer = 1
+//     } else {
+//         answer = -1
+//     }
+//     return answer;
+// }
