cbc8c83f5d159c18ec9d115acf40dd6dd73cb45b
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-11-28 10:39:20 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -0,0 +1,15 @@
+function solution(array, n) {
+    return array.filter(v => v === n).length
+}
+
+
+// function solution(array, n) {
+//     var answer = 0;
+    
+//     for (i of array) {
+//         if (i === n) {
+//             answer += 1
+//         }
+//     }
+//     return answer;
+// }
