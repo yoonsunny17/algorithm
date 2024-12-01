71df70f984eb5f031d5aa7c4eae814b66ea7ccdf
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-12-01 22:12:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,9 @@
+const solution = (my_string) => {
+    return my_string.split("").reverse().join("")
+}
+
+// function solution(my_string) {
+//     return my_string.split("").reverse().join("");
+// }
+
+
