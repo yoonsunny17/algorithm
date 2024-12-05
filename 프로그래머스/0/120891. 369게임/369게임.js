0aecc088305c27b67523b849e2d55b25e7ad66c3
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-12-05 20:18:21 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,37 +1,10 @@
-// 1. match 정규식 사용하기
-// function solution(order) {
-//     // 369 아무것도 가지고 있지 않는 경우 0 출력해줘!
-//     return order.toString().match(/[369]/g).length ?? 0
-// }
-
-// 2. set, has 사용하기
 function solution(order) {
-    const s = new Set('369')
-    return order.toString().split('').filter(v => s.has(v)).length
+    var answer = 0;
+    const numbs = order.toString().split('')
+    for (numb of numbs) {
+        if (numb == '3' || numb == '6' || numb == '9') {
+            answer += 1
+        }
+    }
+    return answer;
 }
-
-
-// function solution(order) {
-//     let answer = 0;
-//     const orderNum = [...order.toString()]
-    
-//     for (num of orderNum) {
-//         if (['3', '6', '9'].includes(num)) {
-//             answer++
-//         }
-//     }
-    
-//     return answer
-// }
-
-
-// function solution(order) {
-//     var answer = 0;
-//     const numbs = order.toString().split('')
-//     for (numb of numbs) {
-//         if (numb == '3' || numb == '6' || numb == '9') {
-//             answer += 1
-//         }
-//     }
-//     return answer;
-// }
